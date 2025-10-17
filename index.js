71fd48204d05c93861bc2555efce12bedf474909
tnayem <tasdidnayem@gmail.com>
2025-10-17 11:16:12 +0600

    Implement post method

---
+++ index.js
@@ -0,0 +1,38 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://my_db:my_db1122@cluster0.etjzxzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri)
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    app.post('/users',(req,res)=>{
+        console.log("Get data from :",req.body);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`My App is Running from ${port}`)
+})
